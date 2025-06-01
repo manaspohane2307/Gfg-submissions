3bf6bb8543cdd2d7b135be3cae98b2c82a4de540
Manas Pohane <manaspohane@gmail.com>
2025-06-01 12:54:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+class Solution {
+    int longestUniqueSubstring(String s) {
+        // code here
+        int n = s.length();
+        int left = 0;
+        int right = 0;
+        int maxlen = 0;
+        int[] hasharray = new int[256];//will store index at which each character occcurs
+        for (int i = 0; i < 256; i++)
+            hasharray[i] = -1;//initialize all values to -1
+
+        while (right < n) {
+            if (hasharray[s.charAt(right)] != -1) {//found in map
+                if (hasharray[s.charAt(right)] >= left) {
+                    left = hasharray[s.charAt(right)] + 1;//update left pointer
+                }
+            }
+            maxlen = Math.max(maxlen, right - left + 1);
+            hasharray[s.charAt(right)] = right;//store index
+            right++;//update right pointer
+        }
+        return maxlen;
+    }
+}
