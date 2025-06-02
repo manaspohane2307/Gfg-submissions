cd55a04f6a83559979b42e1d90db9cfb4ed280f4
Manas Pohane <manaspohane@gmail.com>
2025-06-02 17:41:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(int n) {
+        // code here
+        if((n & (n-1))==0){
+            return true;
+        }
+        return false;
+        
+    }
+}
