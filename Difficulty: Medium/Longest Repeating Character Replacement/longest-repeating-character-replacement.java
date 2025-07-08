43e4f87acb2080763355fb5fc3331c2d545cbc5f
Manas Pohane <manaspohane@gmail.com>
2025-07-08 16:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+
+class Solution {
+    static int characterReplacement(String s, int k) {
+        // code here
+        int left=0, right=0;
+        int maxlen=0, maxf=0;
+        int n = s.length();
+        int[] hash = new int[26];
+        while(right<n){
+            hash[s.charAt(right)-'A']++;
+            maxf = Math.max(maxf, hash[s.charAt(right)-'A']);
+            if((right-left+1)-maxf > k){
+                hash[s.charAt(left) - 'A']--;
+                maxf = 0;
+                left++;
+            }
+            if((right-left+1)-maxf <= k){
+                maxlen = Math.max(maxlen, right-left+1);
+            }
+            right++;
+        }
+        return maxlen;
+    }
+}
